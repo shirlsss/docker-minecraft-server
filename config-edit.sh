57a5fecdd1ef9678ed3095f5b290b34678ed0b3a
shirlsss <shirleyzheng2001@hotmail.com>
2026-09-19 13:01:30 -0700

    added missing continue in script

--- config-edit.sh
+++ config-edit.sh
@@ -260,6 +260,7 @@ menu() {
 	while [[ ${continue} -eq 1 ]]; do
 		if [[ -s server.properties ]]; then
 			echo -e "Existing config found...\nstarting server\n"
+			continue
 			# read -p "Found an existing config file! Would you like to edit this file? y/n " edit
 			# echo
 		    #    	chmod 777 server.properties	
